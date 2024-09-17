4d42bb7d893b3f4f6dba0d2e407dd4e0a897d6e6
Lorelei Wannyn <loreleiwannyn@MacBook-Air-de-Lorelei.local>
2024-09-17 17:56:05 +0200

    fix

--- sutom.js
+++ sutom.js
@@ -5,6 +5,7 @@ let nombreessais = 0
 let moyenne = 0
 let reset = document.getElementById ( 'reset' )
 let start = document.getElementById ( 'start' )
+let jeuencours = true
 if (localStorage.getItem('compteurwin')) {
     compteurwin = parseInt(localStorage.getItem('compteurwin'))
 }
@@ -17,13 +18,36 @@ if (localStorage.getItem('moyenne')) {
 if (localStorage.getItem('nombreessais')) {
     nombreessais = parseInt(localStorage.getItem('nombreessais'))
 }
-if (compteurwin > 0) {
+if ((compteurwin +compteurloose) > 0) {
     moyenne = Math.round(nombreessais / (compteurwin + compteurloose));
 }
-console.log ( "essais" + nombreessais)
-console.log('moyenne'+ moyenne)
-console.log('win' + compteurwin) //
-console.log('loose'+ compteurloose)
+
+// console.log(jeuencours)
+// Ici on veut que le bouton commencer ne réapparaisse pas à chaque partie ( il reviendra au bouton reset)
+if (localStorage.getItem('jeuencours')) {
+    jeuencours = JSON.parse(localStorage.getItem('jeuencours'))
+}
+    // bouton start qui enlève le filtre blur du body
+start.addEventListener("click", function() {
+   
+    document.querySelector(".container").style.filter = "none";
+    document.querySelector("#start").style.display = "none";
+    jeuencours = false
+    localStorage.setItem('jeuencours', jeuencours);
+    // console.log(jeuencours);
+    ;
+});
+if ( jeuencours === false) {
+    document.querySelector(".container").style.filter = "none";
+    document.querySelector("#start").style.display = "none";
+    } ;
+    // console.log(jeuencours)
+
+
+// console.log ( "essais" + nombreessais)
+// console.log('moyenne'+ moyenne)
+// console.log('win' + compteurwin) //
+// console.log('loose'+ compteurloose)
 vic = document.getElementById('victoires')
 vic.innerHTML = compteurwin
 loss = document.getElementById('defaites')
@@ -32,45 +56,55 @@ moy = document.getElementById('tentatives')
 moy.innerHTML = moyenne
 ////////////////////////////////
 
-
-// création d'une liste de mots 
-let words = ["pomme", "banane", "cerise", "noix", "ananas", "éléphant", "crocodile", "chenille", "chat", "escargot", "noël","carnaval","cuisine","service","festival",];
-// console.log(words);
+fetch("https://trouve-mot.fr/api/random")
+    .then((response) => response.json())
+    .then((words) => {
+        let random = words[0].name;
+        // console .log(random);
+        // création d'une liste de mots 
+//let words = ["pomme", "banane", "cerise", "noix", "ananas", "éléphant", "crocodile", "chenille", "chat", "escargot", "noël","carnaval","cuisine","service","festival",];
+// // console.log(words);
 // Création d'une fonction pour choisir un mot du tableau 
-function randomword(words) {
+/* function randomword(words) {
     return words[Math.floor(Math.random() * words.length)];
-}
+} */
 // stocker le mot aléatoire dans une variable
-let random = randomword(words);
+//let random = randomword(words);
 let majuscule = random.toUpperCase()
-// console.log(random);
+// console.log(majuscule);
+
 let sansAccents = majuscule.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+// console.log(sansAccents);
 // découper le mot en tableau et stocker chaque lettre dans une variable 
 let letters = sansAccents.split("");
-// console.log(letters);
+ // console.log(letters);
 // compter le nombre d'élément dans letters
 let count = letters.length;
-// console.log(count);
+// // console.log(count);
+   
+
+
+
 // créer un tableau dans la div grid
 let grid = document.getElementById("grid")
 let table = document.createElement("table");
 grid.appendChild(table);
 // création de 6 lignes pour chaque lettre tableau
 for (i = 0; i < 6; i++) {
-    // // console.log(i);
+    // // // console.log(i);
     let row = document.createElement("tr")
     row.setAttribute("id", "try" + i)
     table.appendChild(row)
     for (j = 0; j < letters.length; j++) {
-        // // console.log(j);
+        // // // console.log(j);
         let cell = document.createElement("td")
         cell.setAttribute("id", "letter" + i + j)
         row.appendChild(cell)
         if (j === 0 ) {
             let first = letters[0]
             cell.textContent  = first
-            // console.log(cell);
-            // console.log(first);
+            // // console.log(cell);
+            // // console.log(first);
             
         }
         cell = letters[j]
@@ -81,7 +115,7 @@ for (i = 0; i < 6; i++) {
 let currentRow = 0;
 let currentCol = 1;
 highlightCurrentCell(currentRow, currentCol); // Mettre à jour la surbrillance
-// console.log(currentRow +","+ currentCol)
+// // console.log(currentRow +","+ currentCol)
 // Sélectionner toutes les divs avec un attribut data-lettre
 let lettreDivs = document.querySelectorAll('div[data-lettre]');
 // Boucle à travers chaque div et ajout de l'écouteur d'événements
@@ -90,11 +124,11 @@ lettreDivs.forEach(div => {
         
         // Récupérer la valeur de l'attribut data-lettre
         let lettre = div.getAttribute('data-lettre');
-        // console.log('Lettre cliquée :', lettre);
+        // // console.log('Lettre cliquée :', lettre);
         //test pour voir si lettre récupère bien la valeur        // Effacer la case précédente si on appuie sur effacer
         if (lettre === "_effacer") {
-            // console.log("EFFACE MOI CA");
-            // console.log(currentRow + "," + currentCol)
+            // // console.log("EFFACE MOI CA");
+            // // console.log(currentRow + "," + currentCol)
             let cell = document.getElementById("letter" + currentRow + (currentCol - 1))
             if (currentCol > 0) {
                 currentCol--;
@@ -104,7 +138,7 @@ lettreDivs.forEach(div => {
             else { return }
             cell.textContent = "";
 
-            // console.log(currentRow + "," + currentCol)
+            // // console.log(currentRow + "," + currentCol)
         } else {
             if (currentCol < letters.length && lettre === "_entree") {
                 return
@@ -115,11 +149,14 @@ lettreDivs.forEach(div => {
                 
                 currentCol++; // Passer à la prochaine colonne
                  highlightCurrentCell(currentRow, currentCol); // Mettre à jour la surbrillance
-                // console.log(currentRow + "," + currentCol)
+                // // console.log(currentRow + "," + currentCol)
             }
                // Si la ligne est terminée, passer à la ligne suivante et vérification de placement et victoire
                if (currentCol === letters.length && lettre === "_entree") 
-                { verifyredAndOrange(letters, currentRow)
+                { 
+                   
+                    
+                    verifyredAndOrange(letters, currentRow)
                   let victoire = win(letters, currentRow);
                   nombreessais ++;
                   localStorage.setItem('nombreessais', nombreessais);
@@ -142,12 +179,12 @@ addEventListener('keydown', function (event) {
     // Convertir la touche en majuscule
     let maj = event.key;
     let lettre = maj.toUpperCase();
-    console.log('Touche pressée :', event.key);
+    // console.log('Touche pressée :', event.key);
     
     // Vérifier si c'est la touche "Backspace" pour effacer
     if (event.key === "Backspace") {
         lettre = "_effacer";
-        console.log('Lettre :', lettre);
+        // console.log('Lettre :', lettre);
 
     }
 
@@ -275,7 +312,7 @@ function verifyredAndOrange(letters, currentRow) {
     // Première passe : vérifier et marquer les lettres correctement placées (rouge)
     for (let col = 0; col < letters.length; col++) {
         let cell = document.getElementById("letter" + currentRow + col);
-        let lalettre = cell.textContent; // on récupère la lettre pour la décompter après
+        let lalettre = cell.textContent; 
         currentAttempt.push(lalettre); // Ajouter la lettre dans la tentative actuelle
         let keyboardKey = document.querySelector(`div[data-lettre='${lalettre}']`); //ajoute la lettre qui a été tapé dans la variable
         // Si la lettre est à la bonne position, on la marque comme correcte (rouge)
@@ -286,9 +323,9 @@ function verifyredAndOrange(letters, currentRow) {
             
             
             // Si on a atteint la dernière cellule de la ligne actuelle
-            if (col === letters.length - 1) {
-                return false; // Arrêter l'exécution
-            }
+           // if (col === letters.length - 1) {
+             //   return ; // Arrêter l'exécution
+           // }
         }
     }
     // Deuxième passe : vérifier les lettres mal placées (orange) ou incorrectes
@@ -297,6 +334,7 @@ function verifyredAndOrange(letters, currentRow) {
         let lalettre = cell.textContent; // on récupère la lettre pour la décompter après 
         let keyboardKey = document.querySelector(`div[data-lettre='${lalettre}']`);
         // Si la lettre n'est pas correcte et est ailleurs dans le mot
+        // console .log(lalettre);// on récupère la lettre pour la décompter après
         if (cell.getAttribute("class") !== "correct" && letters.includes(lalettre)) {
             // Si la lettre a encore des occurrences disponibles (répétitions restantes)
             if (repetitions[lalettre] > 0) {
@@ -308,11 +346,7 @@ function verifyredAndOrange(letters, currentRow) {
                 cell.setAttribute("class", "incorrect");
                 keyboardKey.setAttribute("class", "incorrectkey");
             }
-        } else if (cell.getAttribute("class") !== "correct") {
-            // Si la lettre n'est ni correcte ni mal placée, la marquer comme incorrecte
-            cell.setAttribute("class", "incorrect");
-            keyboardKey.setAttribute("class", "incorrectkey");
-        }
+        } 
     }
 
 };
@@ -335,6 +369,7 @@ reset.addEventListener("click", function() {
  compteurloose = 0
  nombreessais = 0
  moyenne = 0
+ jeuencours = true
         // Mise à jour de l'affichage
         vic.innerHTML = compteurwin;
         loss.innerHTML = compteurloose;
@@ -345,12 +380,10 @@ reset.addEventListener("click", function() {
         localStorage.removeItem('compteurloose');
         localStorage.removeItem('nombreessais');
         localStorage.removeItem('moyenne');
+        localStorage.setItem('jeuencours',jeuencours);
+        location.reload();
 })
-// bouton start qui enlève le filtre blur du body
-start.addEventListener("click", function() {
-    document.querySelector(".container").style.filter = "none";
-    document.querySelector("#start").style.display = "none";
-});
+
 
  
 
@@ -400,3 +433,35 @@ function rewriteCorrectLetters(currentRow) {
     }
 };
 
+})
+
+//// DICTIONNAIRE ////////
+
+// Fonction pour lire le fichier local
+function readFile() {
+    return fetch('larousse.txt')
+      .then(response => response.text()) 
+      .then(text => text.split('\n')); // j'utilise \ car chaque mot est sur une ligne 
+      
+  };
+  
+  // Fonction pour vérifier si le mot existe
+  function verifyWord(word) {
+    return fetch('/words')
+      .then(response => response.json())
+      .then(words => words.includes(word.toUpperCase()));
+  };
+  
+
+
+  // Fonction pour recomposer le mot formé sur la ligne actuelle
+function getCurrentRowWord(row) {
+    let word = '';
+    for (let col = 0; col < letters.length; col++) {
+        let cell = document.getElementById("letter" + row + col);
+        word += cell.textContent || ''; // Ajoute le texte de la cellule, ou une chaîne vide si la cellule est vide
+        
+    }
+    return word;
+    
+};
